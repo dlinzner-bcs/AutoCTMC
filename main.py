55bf8e11eb52066a09486341a8673b69af57ee67
Dominik Linzner <dominik.linzner@bcs.tu-darmstadt.de>
2020-07-24 11:14:10 +0200

    clean up

--- main.py
+++ main.py
@@ -98,6 +98,8 @@ def emit_obs_M_norm(mc,M,K):
 
 
 if __name__ == '__main__':
+
+#init ctmc
     T = 21
     dt = 0.0005
     D = 2
@@ -114,91 +116,27 @@ if __name__ == '__main__':
     mu = np.arange(0,D)*1/2
     sig= np.ones((D))*0.1
     params = (mu,sig)
-    print(params[0][1])
-
     mc = ctmc(Q,p0,alpha,beta,T,dt,params)
 
-    z = mc.sample()
-    t_lh = np.array([0])
-    t0 = 0
-    lh = np.ones((mc.dims,len(z)))
-    p=0.5
-    for i in range(0, len(z)):
-        t0 = t0 + z[i][0]
-        if np.random.uniform(low=0,high=1)<p:
-          t_lh = np.concatenate((t_lh,t0*np.ones((1))))
-          for j in range(0,mc.dims):
-             lh[j,i] = obs_lh(z[i][1],j)
-
-    M = 200
-    err = np.zeros((M,1))
-    for k in range(0,M):
-        p0 = np.ones((1, D)).flatten()
-        p0[np.random.randint(low=0,high=D)] = 0
-        p0 = p0 / sum(p0)
-        mc.set_init(p0)
-        z = mc.sample()
-        mc.update_statistics(z)
-        mc.estimate_Q()
-        a = copy.copy(mc.Q_estimate)
-        b = copy.copy(mc.Q)
-        np.fill_diagonal(a,0)
-        np.fill_diagonal(b,0)
-        err[k] = np.linalg.norm(a-b)
-    plt.figure(10)
-    plt.plot(err)
-    plt.show()
-
-    sol = mc.forward_ode()
-    y = sol.y
-    t_y = sol.t
-    plt.figure(2)
-    plt.plot(t_y, y[0, :])
-    plt.plot(t_y, y[1, :])
-
-    mc.reset_stats()
-    mc.estimate_Q()
+#generate data
 
     M = 100
     K   = 20
     dat = gen_obs_M(mc, 20, K)
 
-    # for m in range(0,M):
-    #     for k in range(0,K):
-    #         mc.process_dat(dat[k][0],dat[k][1])
-    #     print(mc.trans)
-    #     print(mc.llh())
-    #     mc.estimate_Q()
-    #     mc.reset_stats()
-    #
-    #     a = copy.deepcopy(mc.Q_estimate)
-    #     b = copy.copy(mc.Q)
-    #     a0 = copy.deepcopy(mc.Q_estimate)
-    #     np.fill_diagonal(a0, 0)
-    #     np.fill_diagonal(b, 0)
-    #     print(a0)
-    #     print(b)
-    #     print(np.linalg.norm(a0 - b))
-    #     err[m] = np.linalg.norm(a0 - b)
+#estimate rate matrix and obs model (only means atm)
 
+    mc.reset_stats()
+    mc.estimate_Q()
     dat = emit_obs_M_norm(mc, 5, K)
     for m in range(0, M):
         llh, sols = mc.process_emissions(dat)
         mc.update_obs_model(sols, dat)
-        # def mu_max(x):
-        #     mc0 =copy.copy(mc)
-        #     params = (x,mc.params[1])
-        #     mc0.params = params
-        #     llh = mc0.llh_dat_full(sols, dat)
-        #     return -llh
-        # res = minimize(mu_max,x0 = mc.params[0],method='Nelder-Mead', tol=1e-6)
-        # x = res.x
-        # params = (x, mc.params[1])
-        # mc.params = params
+
+        #current obs params estimate
         print(mc.params[0])
 
         mc.estimate_Q()
-
         mc.reset_stats()
 
         a = copy.deepcopy(mc.Q_estimate)
@@ -206,8 +144,8 @@ if __name__ == '__main__':
         a0 = copy.deepcopy(mc.Q_estimate)
         np.fill_diagonal(a0, 0)
         np.fill_diagonal(b, 0)
+        # mse of rate matrix estimate
         print(np.linalg.norm(a0 - b))
-        err[m] = np.linalg.norm(a0 - b)
 
 
 
