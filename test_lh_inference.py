b9cf63b924ec70269f1698c45c941176cda030a8
Dominik Linzner <dominik.linzner@bcs.tu-darmstadt.de>
2020-07-24 15:35:34 +0200

    removed unecessary file

--- test_lh_inference.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import numpy as np
-import matplotlib.pyplot as plt
-from ctmc.ctmc import ctmc
-from scipy.stats import norm
-import copy
-
-from main import obs
-from main import emit_obs_M_norm
